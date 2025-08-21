0b1ccda36d3277ac06cbfc4019817fe1b2068b6e
Renchs <mathfm.dev@gmail.com>
2025-08-21 11:59:17 -0300

    refactor: condicional questão 6

--- src/implementacao/6-votacao.ts
+++ src/implementacao/6-votacao.ts
@@ -1,19 +1,30 @@
 function apuraVotacao(votos: Array<number>) {
+  let candidato1 = 0;
+  let candidato2 = 0;
+  let candidato3 = 0;
 
-    let candidato1 = 0;
-    let candidato2 = 0;
-    let candidato3 = 0;
+    for (let i = 0; i < votos.length; i++) {
+        if (votos[i] === 1) {
+            candidato1++;
+        } else if (votos[i] === 2) {
+            candidato2++;
+        } else if (votos[i] === 3) {
+            candidato3++;
+        }
+    }
 
-    votos.map((voto) => {
-        voto == 1 ? candidato1++ :
-        voto == 2 ? candidato2++ :
-        voto == 3 ? candidato3++ : null;
-    })
+    if (candidato1 > candidato2 && candidato1 > candidato3) {
+        return "Vencedor: 1";
+    }
 
-    const result = candidato1 > candidato2 && candidato1 > candidato3 ? "Vencedor: 1" :
-    candidato2 > candidato1 && candidato2 > candidato3 ? "Vencedor: 2" :
-    candidato3 > candidato2 && candidato3 > candidato1 ? "Vencedor: 3" : "Empate";
+    else if (candidato2 > candidato1 && candidato2 > candidato3) {
+        return "Vencedor: 2";
+    }
 
-    return result;
+    else if (candidato3 > candidato1 && candidato3 > candidato2) {
+        return "Vencedor: 3";
+    }
+
+  return "Empate";
 }
 module.exports = apuraVotacao;
